e2945e5926ecdbd5e409a3ec5df8a14828543d57
Cameron Sewell <77357348+CameronSewell@users.noreply.github.com>
2022-11-15 16:11:52 -0700

    Player actions on client side (#19)
    * created player object
    
    * added communcation logic
    
    * updated player class
    
    * updated imports

---
+++ client/player.go
@@ -0,0 +1,60 @@
+package client
+
+import (
+	"fmt"
+	"log"
+	"main/message"
+	"main/cards"
+	"main/Dealer"
+	"golang.org/x/net/websocket"
+)
+
+type player interface
+{
+	hit()
+	stand()
+	quit()
+	bet(int)
+
+	var Total int
+	var hand []cards.cards
+	var money float32
+}
+
+func stand()
+{
+	var data message.T
+	data.Msg = "call stand()"
+	if err := websocket.JSON.Send(ws, data); err != nil {
+		log.Fatal(err)
+	}
+	if err = websocket.JSON.Receive(ws, &data); err != nil {
+		log.Fatal(err)
+	}
+}
+func hit()
+{
+	var data message.T
+	data.Msg = "dealer.drawCard()"
+	if err := websocket.JSON.Send(ws, data); err != nil {
+		log.Fatal(err)
+	}
+	if err = websocket.JSON.Receive(ws, &data); err != nil {
+		log.Fatal(err)
+	}
+}
+func quit()
+{
+
+}
+func bet(int amount)
+{
+	var data message.T
+	data.Msg = "call bet(amount)"
+	if err := websocket.JSON.Send(ws, data); err != nil {
+		log.Fatal(err)
+	}
+	if err = websocket.JSON.Receive(ws, &data); err != nil {
+		log.Fatal(err)
+	}
+}
